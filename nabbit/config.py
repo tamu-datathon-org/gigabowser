24f7b9695833f61e0244ee08767265e8441dfad1
Daniel Abreo <34114190+danielabreo@users.noreply.github.com>
2020-02-15 13:15:09 -0600

    created nabbit

---
+++ nabbit/config.py
@@ -0,0 +1,4 @@
+pathToNabbit = "/Users/dan/Downloads/nabbit"
+repoPath = pathToNabbit+"/gitRepo"
+gitLink = "git@github.com:danielabreo/danielabreo.github.io.git"
+siteLink = "https://olusola-babatundes-dandy-project.webflow.io"
