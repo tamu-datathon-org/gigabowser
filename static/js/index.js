9160693e88df3c026b80bfd64fd47a39e71d8d6b
phulsechinmay <phulsechinmay@gmail.com>
2020-04-19 14:21:00 -0500

    Changed to javascript-based scrolling. Scroll-to-ids are defined in NavbarVariables.

---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+$('.same-page-nav-link').click((e) => {
+  const scrollToSection = $(e.target).attr('scroll-id');
+  if (scrollToSection)
+    $('html, body').animate({
+      scrollTop: $(`#${scrollToSection}`).offset().top
+    }, 1000);
+})
