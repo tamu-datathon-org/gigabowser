6766f5113cc25a9f92133fd5b62416545acfb2f5
gcpetri <gcpetri@outlook.com>
2021-05-18 23:15:36 -0500

    carousel looking ~fly~

--- static/js/events.js
+++ static/js/events.js
@@ -5,29 +5,5 @@ $('.event-carousel').slick({
   autoplaySpeed: 3000,
   prevArrow: $('.event-prev'),
   nextArrow: $('.event-next'),
-  //dots: true,
-  /*
-  centerMode: true,
-  centerPadding: '60px',
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 4
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
-  ]
-  */
+  // dots: true,
 });
