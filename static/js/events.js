a9cd0bc31509a34d000a19ca214d92a439648dc1
jacksebastian17 <jacksebastian17@tamu.edu>
2021-05-26 11:19:10 -0500

    changed event carousel so that part of some slides cut off the indicate theres more events hidden

--- static/js/events.js
+++ static/js/events.js
@@ -1,9 +1,29 @@
 $(".event-carousel").slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
   autoplay: true,
   autoplaySpeed: 3000,
   prevArrow: $(".event-prev"),
   nextArrow: $(".event-next"),
-  // dots: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
 });
