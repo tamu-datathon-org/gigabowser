fb4a0851d8c29eec9f195a5ba60a025676fd2c34
Gregory Petri <gcpetri@outlook.com>
2022-05-23 17:09:25 -0500

    team page updates

--- static/js/partials/team.js
+++ static/js/partials/team.js
@@ -1,7 +1,11 @@
 class Team extends Section {
   constructor(id, startPx, height, halt = defaultHalt, isFirst = false) {
-    // height = 1200; // override default height
+    const teamContent = document.getElementById(id);
+    height = teamContent.getBoundingClientRect().height; // override default height
     super(id, startPx, height, halt, isFirst);
+    teamContent.querySelectorAll('.clue-card').forEach(card => {
+      card.onclick = (e) => this.cardFlip(e, card);
+    });
   }
   runScrollDependentBehavior(y) {
     // put scroll dependent behavior here
@@ -9,6 +13,8 @@ class Team extends Section {
   run(y) {
     super.run(y);
   }
+  cardFlip(e, card) {
+    card.querySelector('.clue-card-inner').classList.toggle('flipped');
+  }
 }
-
 // put interaction based code under here (button clicks, hover event, etc)
