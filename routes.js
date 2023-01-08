891712a5de9a5f8935d60e50574c48e8b8c0d8f8
Christopher Vu <65040991+S1nthesis@users.noreply.github.com>
2023-01-07 20:25:47 -0600

    commented out challenges in routes

--- routes.js
+++ routes.js
@@ -15,7 +15,7 @@ app.get("/livestream-timer", renderPage("livestream-timer", "livestream-timer"))
 // app.get("/events", renderPost("./markdown/events.md", "markdown_layout"));
 app.get("/schedule", renderPost("./markdown/schedule.md", "markdown_layout_wide"));
 app.get("/legal/talent_liability_terms", renderPost("./markdown/legal/talent_liability_terms.md", "markdown_layout"));
-app.get("/challenges", renderPost("./markdown/challenges.md", "markdown_layout"));
+// app.get("/challenges", renderPost("./markdown/challenges.md", "markdown_layout"));
 app.get("/apply", renderPost("./markdown/apply.md", "markdown_layout"));
 app.get("/volunteer", renderPost("./markdown/volunteer.md", "markdown_layout"));
 
