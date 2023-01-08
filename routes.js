661b6bda3b5ef700c9e985e3479cf65a6516dc9e
Christopher Vu <65040991+S1nthesis@users.noreply.github.com>
2023-01-07 20:41:50 -0600

    comment out challenges route

--- routes.js
+++ routes.js
@@ -14,7 +14,7 @@ app.get("/twitch", renderPage("twitch", "twitch"));
 app.get("/events", renderPost("./markdown/events.md", "markdown_layout"));
 app.get("/schedule", renderPost("./markdown/schedule.md", "markdown_layout_wide"));
 app.get("/legal/talent_liability_terms", renderPost("./markdown/legal/talent_liability_terms.md", "markdown_layout"));
-app.get("/challenges", renderPost("./markdown/challenges.md", "markdown_layout"));
+// app.get("/challenges", renderPost("./markdown/challenges.md", "markdown_layout"));
 app.get("/apply", renderPost("./markdown/apply.md", "markdown_layout"));
 
 exports.app = app;
